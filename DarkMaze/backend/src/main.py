8f4b74a3978745ed04036cbb41e87d87ef8a8187
WANG-YongLE <a2a45a789@gmail.com>
2025-03-10 01:17:10 +0800

    feat:DarkMaze

--- DarkMaze/backend/src/main.py
+++ DarkMaze/backend/src/main.py
@@ -1,10 +1,7 @@
-from datetime import datetime, timedelta
 from fastapi import FastAPI, Request, Response, Depends
-from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from .database.initialize import initialize
-from .database.operation import create_user, get_latest_game_state, reset_game_state
-from .game.operation import move_location
+
 from .auth.router import router as auth_router
 from .game.router import router as game_router
 import uvicorn
@@ -12,8 +9,6 @@ app = FastAPI()
 app.include_router(auth_router)
 app.include_router(game_router)
 initialize()
-
-FRONTEND_URL = "http://localhost:8088"
 
 app.add_middleware(
     CORSMiddleware,
